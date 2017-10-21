584eaf9cb3b68024233e9b6be1a2f7a728bb2aba
Adrián Tóth <adricko28@gmail.com>
2017-10-21 14:13:20 +0200

    list ONLY files from directory

--- test.cpp
+++ test.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-#include <fstream>
-#include <sstream>
+#include <dirent.h>
+#include <sys/stat.h> // file_exists
 
-std::string file_size(std::string filename)
-{
-    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
-    std::stringstream ss;
-    std::string str;
-    ss << in.tellg();
-    ss >> str;
-    return str; 
+// Check if FILE exists
+bool file_exists(const std::string path) {
+    struct stat s;
+    if((stat(path.c_str(),&s)==0)&&(s.st_mode & S_IFREG )) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+std::vector<std::string> list_dir(std::string dirpath) {
+
+    std::vector<std::string> files;
+    std::string data;
+
+    struct dirent *entry;
+    DIR *dir = opendir(dirpath.c_str());
+    if (dir == NULL) {
+        return files;
+    }
+    while ((entry = readdir(dir)) != NULL) {
+        data = entry->d_name;
+        if (file_exists(data)) {
+            data = realpath(data.c_str(), NULL); // TODO FIXIT realpath may return NULL
+            files.push_back(data);
+        }
+    }
+    closedir(dir);
+
+    return files;
 }
 
 int main(int argc, char* argv[]) {
     
     if (argc != 2) {
-        fprintf(stderr, "USAGE: ./test <filepath>");
+        fprintf(stderr, "USAGE: ./test <dirpath>");
         exit(1);
     }
 
-    std::string size = file_size(argv[1]);
-
-    std::cout << size;
+    std::vector<std::string> files;
+    files = list_dir(argv[1]);
+    if (!files.empty()) {
+        for (auto i = files.begin(); i != files.end(); ++i) {
+            std::cout << *i << std::endl;
+        }
+    }
 
     return 0;
 }
