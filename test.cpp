5e7f70243c1e2cb62d29a74ea8a58de26c25f7e2
Adrián Tóth <adricko28@gmail.com>
2017-11-10 19:37:40 +0100

    file_size() fix for \n => 2chars => sending \r\n instead of \n

--- test.cpp
+++ test.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+#define PRINT(data) std::cout << "==>" << data << "<==" << std::endl << std::endl
+
+// Function get the file size in octets
+std::string file_size(std::string filename) {
+    long long unsigned int size = 0;
+    bool ENDS_WITH_NEWLINE = false;
+
+    std::ifstream in(filename);
+    std::string str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+    if (str.back() == '\n') ENDS_WITH_NEWLINE = true;
+    
+    std::ifstream infile(filename, std::ifstream::ate | std::ifstream::binary);
+    size = infile.tellg();
+
+    std::string line;
+    std::ifstream file(filename);
+    std::string buff;
+    while(std::getline(file, line)) {
+        buff = (line.back() == '\r') ? line.substr(0, line.size()-1) : line;
+        PRINT(buff);
+        if (line.back() != '\r') {
+            size++;
+        }
+    }
+
+    if (!ENDS_WITH_NEWLINE) {
+        size--;
+    }
+
+    return std::to_string(size);
+}
+
 
 int main(int argc, char* argv[]) {
 
+    if (argc != 2) exit(1);
+    std::cout << file_size(argv[1]) << std::endl;
+
     return 0;
 }
+    
